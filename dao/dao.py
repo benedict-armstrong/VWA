0948da17092557247d9a1b69a0578405ff947b54
Ben Armstrong <ben@Bens-MacBook-Pro.local>
2017-08-09 20:54:21 +0200

    edited the dao

--- dao/dao.py
+++ dao/dao.py
@@ -35,3 +35,28 @@ def remove_movie(movie_id):
     with get_db_cursor() as cursor:
         cursor.execute("""delete from movie where id=%s;""", [movie_id])
         return cursor.fetchone()
+
+def get_movie(movie_id):
+    with get_db_cursor() as cursor:
+        cursor.execute("""select * from movie where id=%s;""", [movie_id])
+        return cursor.fetchone()
+
+def get_movies_showing():
+    with get_db_cursor() as cursor:
+        cursor.execute("""select movie_id from screening group by movie_id;""")
+        return cursor.fetchall()
+
+def get_screenings(movie_id):
+    with get_db_cursor() as cursor:
+        cursor.execute("""select * from screening where movie_id=%s;""", [movie_id])
+        return cursor.fetchall()
+
+def room_get_seats(screening_id, room_id):
+     with get_db_cursor() as cursor:
+        cursor.execute("""select *,(select id from ticket where ticket.seat_id = seat.id and booking_id = (select id from booking where screening_id = '%s')) from seat where room_id='%s'""", [screening_id, room_id])
+        return cursor.fetchall()
+        
+def create_booking(customer_id, screening_id):
+    with get_db_cursor() as cursor:
+        cursor.execute("""insert into booking (screening_id, customer_id) values (%s,%s) returning booking_reference;""", [screening_id, customer_id])
+        return cursor.fetchone()
